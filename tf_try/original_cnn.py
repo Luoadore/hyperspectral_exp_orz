9d5b2dcd6680882332d8353f76adeeb381f0a87e
Luoadore <luoluomayday@gmail.com>
2017-09-27 22:44:28 +0800

    original net model

--- tf_try/original_cnn.py
+++ tf_try/original_cnn.py
@@ -31,12 +31,12 @@ def inference(dataset, conv1_uints, conv1_kernel, conv1_stride, mpool_uints, fc_
         fc_units:Size of the fully connection layer
     
     Returns:
-        softmax_linear: Output tensor with the computed logits
+        softmax_re: Output tensor with the computed logits
     """
     #conv1
     with tf.name_scope('conv1'):
         weights = tf.Varibale(
-            tf.truncated_normal([1, conv1_kernel, 1, conv1_units],
+            tf.truncated_normal([1, conv1_kernel, 1, conv1_uints],
                                 stddev = 1.0 / math.sqrt(float(conv1_uints))),
             name = 'weights')
         biases = tf.Varibale(tf.zeros(conv1_uints),
@@ -54,4 +54,80 @@ def inference(dataset, conv1_uints, conv1_kernel, conv1_stride, mpool_uints, fc_
     
     #fc
     with tf.name_scope('fc'):
-        weights = 
+        input_uints = (BANDS_SIZE - conv1_kernel + 1) / 2
+        weights = tf.Varibale(
+            tf.turncated_normal([input_uints, fc_uints],
+                                stddev = 1.0 / math.sqrt(float(input_uints))),
+            name = 'weights')
+        biases = tf.Varibale(tf.zeros([fc_uints]),
+                             name = 'biases')
+        mpool_flat = tf.reshape(mpool, [-1, input_uints])
+        fc = tf.nn.relu(tf.matmul(mpool_flat, weights) + biases)
+    
+    #softmax regression
+    with tf.name_scope('softmax_re'):
+        weights = tf.Varibale(
+            tf.truncated_normal([fc_uints, NUM_CLASSES],
+                                stddev = 1.0 / math.sqrt(float(fc_uints))),
+            name = 'weights')
+        biases = tf.Varibale(tf.zeros([NUM_CLASSES]),
+                             name = 'biases')
+        softmax_re = tf.nn.softmax(tf.matmul(fc, weights) + biases)
+        
+    return softmax_re
+    
+def loss_acc(softmax_re, labels):
+    """Calculates the loss.
+    
+    Args:
+        softmax_re: net forward output tensor, float - [batch_size, NUM_CLASSES]
+        labels: labels tensor, 
+        
+    Return:
+        entroy: Loss tensor of type float
+    """
+    
+    #loss
+    with tf.name_scope('loss'):
+        entroy = tf.reduce_mean(
+            -tf.reduce_sum(labels * tf.log(softmax_re),
+            reduction_indices = [1]))
+        
+    return entroy
+    
+def acc(softmax_re, labels):
+     """Calculates the accuracy.
+    
+    Args:
+        softmax_re: net forward output tensor, float - [batch_size, NUM_CLASSES]
+        labels: labels tensor, 
+        
+    Return:
+        accuracy: classification accuracy
+    """
+    
+    #accuarcy
+    with tf.name_scope('accuracy'):
+        correct_prediction = tf.equal(tf.argmax(softmax_re, 1), tf.argmax(labels,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        
+    return accuracy
+    
+def training(loss, learning_rate):
+    """Sets up the training Ops.
+    
+    Args:
+        loss: Loss tensor, from loss_acc()
+        learning_rate: The learning rate to use for gradient descent.
+        
+    Returns:
+        train_op: The Op for training
+    """
+    
+    #Add a scalar summary for the snapshot loss. Creates a summarizer to track the loss over time in TensorBoard.
+    tf.scalar_summary(loss.op.name, loss)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    global_step = tf.Variable(0, name = 'global_step', trainable = False)
+    train_op = optimizer.minimize(loss, global_step = global_step)
+    
+    return train_op
