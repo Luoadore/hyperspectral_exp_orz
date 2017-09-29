151acee5ef79f3ff7465a0491d79c7701d049dd9
Luoadore <luoluomayday@gmail.com>
2017-09-29 23:08:28 +0800

    train original method

--- tf_try/train_original.py
+++ tf_try/train_original.py
@@ -2,6 +2,7 @@
 """Train the original network using a feed dictionary."""
 
 import tensorflow as tf
+import original_cnn as oc
 
 def placehoder_inputs(batch_size):
     """Generate palcehold variables to represent the input tensors.
@@ -13,4 +14,5 @@ def placehoder_inputs(batch_size):
         data_placeholder: Data placeholder
         labels_placeholder: Labels placeholder
     """
-    
+    data_placeholder = tf.placeholder(tf.float32, shape = (batch_size, 1 * oc.BANDS_SIZE))
+    label_placeholder = tf.placeholder(tf.int32, shape = (batch_size, 1 * oc.NUM_CLASSES))
