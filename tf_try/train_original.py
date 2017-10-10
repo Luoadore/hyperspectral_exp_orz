13aec7bd1870e70a1ba5572c796c871a272033d7
Luoadore <luoluomayday@gmail.com>
2017-10-10 22:35:46 +0800

    train original method

--- tf_try/train_original.py
+++ tf_try/train_original.py
@@ -2,8 +2,10 @@
 """Train the original network using a feed dictionary."""
 
 import tensorflow as tf
+import time
+import os.path
 import original_cnn as oc
-import data_preprocrssing as dp
+import data_preprocessing as dp
 
 #Basic model parameters as external flags
 flags = tf.app.flags
@@ -37,7 +39,32 @@ def placeholder_inputs(batch_size):
     
     return data_placeholder, label_placeholder
     
-def fill_feed_dict(data_set, label_set, data_pl, label_pl):
+def next_batch(batch_size, num_step, data_set, label_set):
+    """Return the next 'batch_size' examples from the data set.
+    
+    Args:
+        batch_size: The batch size
+        num_step: The step of iteration
+        data_set: The data set
+        label_set: The correspoding label set
+        
+    Return:
+        batch_data: Next batch size data
+        batch_label: Next batch size correspoding label
+    """
+    data_size = len(data_set)
+    num_per_epoch = data_size // batch_size
+    remainder = num_step % num_per_epoch
+    if remainder == 0:
+        data_set, label_set = dp.shuffling2(data_set, label_set)
+    
+    start_index = remainder * batch_size
+    end_index = min(start_index + batch_size, data_size)
+    batch_data = data_set[start_index, end_index]
+    batch_label = label_set[start_index, end_index]
+    yield batch_data, batch_label
+    
+def fill_feed_dict(num_step, data_set, label_set, data_pl, label_pl):
     """Fills the feed_dict for training the given step.
     
     A feed_dict takes the form of:
@@ -47,6 +74,7 @@ def fill_feed_dict(data_set, label_set, data_pl, label_pl):
     }
     
     Args:
+        num_step: The step of iteration
         data_set: The set of data, from dp.load_data()
         label_set: The set of label, from dp.load_data()
         data_pl: The data placeholder, from placeholder_inputs()
@@ -55,14 +83,36 @@ def fill_feed_dict(data_set, label_set, data_pl, label_pl):
     Returns:
         feed_dict: The feed dictionary mapping from placeholders to values.
     """
-    data_feed = data_set.next_batch(FLAGS.batch_size)
-    label_feed = label_set.next_batch(FLAGS.batch_size)
+    data_feed, label_feed = next_batch(FLAGS.batch_size, num_step, data_set, label_set)
     feed_dict = {
                  data_pl: data_feed,
                  label_pl: label_feed,
     }
     
     return feed_dict
+    
+def do_eval(sess, eval_correct, data_placeholder, label_placeholder, data_set, label_set):
+    """Runs one evaluation against the full epoch of data.
+    
+    Args:
+        sess: The session in which the model has been trained
+        eval_correct: The tensor that returns the number of correct predictions
+        data_placeholder: The data placeholder
+        label_placeholder: The label placeholder
+        data_set: The set of data to evaluate, from dp.load_data()
+        label_set: The set of label to evaluate, from dp.load_data()
+        
+    """
+    
+    #And run one apoch of eval
+    true_count = 0
+    num_examples = len(label_set)
+    steps_per_epoch = num_examples // FLAGS.batch_size
+    for step in range(steps_per_epoch):
+        feed_dict = fill_feed_dict(data_set, label_set, data_placeholder, label_placeholder)
+        true_count += sess.run(eval_correct, feed_dict = feed_dict)
+    precision = true_count / num_examples
+    print('Num examples: %d Num correct: %d Precision @ 1: %0.04f' % (num_examples, true_count, precision))
     
 def run_training():
     """Train net model."""
@@ -96,3 +146,39 @@ def run_training():
         #Run the Op to initialize the variables
         sess.run(init)
         
+        #Start the training loop
+        for step in range(FLAGS.max_step):
+            start_time = time.time()
+            
+            feed_dict = fill_feed_dict(step, train_data, train_label, data_placeholder, label_placeholder)
+            
+            #Run one step of the model
+            _, loss_value = sess.run([train_op, loss_entroy], feed_dict = feed_dict)
+            
+            duration = time.time() - start_time
+
+            #Write the summaries and print an overview farily often
+            if step % 100 == 0:
+                #Print status to stdout
+                print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+                #Update the events file
+                summary_str = sess.run(summary_op, feed_dice = feed_dict)
+                summary_writer.add_summary(summary_str, step)
+                summary_writer.flush()
+                
+            #Save a checkpoint and evaluate the model periodically
+            if(step + 1) % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+                checkpoint_file = os.path.join(FLAGS.train_dir, 'checkpoint')
+                saver.save(sess, checkpoint_file, global_step = step)
+                #Evaluate against the data set
+                print('Training Data Eval:')
+                do_eval(sess, correct, data_placeholder, label_placeholder, train_data, train_label)
+                print('Test Data Eval:')
+                do_eval(sess, correct, data_placeholder, label_placeholder, test_data, test_label)
+                
+def main(_):
+    run_training()
+    
+
+if __name__ == '__main__':
+    tf.app.run()
