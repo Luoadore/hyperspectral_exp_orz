f8a5fb1b69468217cfd30245373814adc6c6dea5
Luoadore <luoluomayday@gmail.com>
2017-11-17 22:48:19 +0800

    plotting use matlab

---
+++ tf_try/matlab_plot/plotting.m
@@ -0,0 +1,4 @@
+%plot curve
+
+root = 'F:/hsi_result/original/PU/lossAndaccuracy_train';
+[steps, acc_1, loss_1] = read_train(root, 1)
---
+++ tf_try/matlab_plot/read_train.m
@@ -0,0 +1,22 @@
+function [steps,acc,loss] = read_train(root, neighbor)
+%Load data set from csv, and return column array.
+%
+%   Args:
+%        root: The root path.
+%        neighbor: Pixel neighbor, include 1, 4, 8.
+%
+%    Return:
+%        steps: Corresponding iterations.
+%        acc: Accuracy of per 100 iterations.
+%        loss: Loss of per 100 iterations.
+
+acc_path = root + '/run_' + str(neighbor) + '-tag-accuracy.csv'
+loss_path = root + '/run_' + str(neighbor) + '-tag-training-loss.csv'
+acc_data = csvread(acc_path,1,1)
+loss_data = csvread(loss_path,1,1)
+steps = acc_data(:,1)
+acc = acc_data(:,2)
+loss = loss_data(:, 2)
+
+end
+
