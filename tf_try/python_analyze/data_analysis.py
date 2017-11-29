b3d6c75ec417aa10a4943ca5ef712a6728ea06d0
Luoadore <luoluomayday@gmail.com>
2017-11-29 18:17:34 +0800

    add plot function

--- tf_try/python_analyze/data_analysis.py
+++ tf_try/python_analyze/data_analysis.py
@@ -101,6 +101,38 @@ def plot_misclassify_mean(data_mean, class_mis, data_mis, data_name):
             plt.clf()
             plt.cla()
 
+def plot_diffData_sameClass(data1_mean, data2_mean, acc_class, data_name):
+    """
+    Plot the same class's mean from different data set.
+
+    Args:
+        data1_mean: First data set mean ndarray.
+        data2_mean: Second data set mean ndarray.
+        acc_class: [i, j, name] which i means the class in first data set and j means the class in second data set, name is the class name.
+        data_name: Plotting figure name.
+
+    """
+    class_num = np.size(acc_class, 0)
+    fea_num_1 = np.size(data1_mean, 1)
+    xidx_1 = range(0, fea_num_1)
+    fea_num_2 = np.size(data2_mean, 1)
+    xidx_2 = range(0, fea_num_2)
+
+    for i in range(class_num):
+        data_1 = data1_mean[acc_class[i][0], :]
+        data_2 = data2_mean[acc_class[i][1], :]
+        plt.plot(xidx_1, data_1, label = 'class' + str(acc_class[i][0]) +  ' in PC')
+        plt.plot(xidx_2, data_2, label = 'class' + str(acc_class[i][1]) + ' in PU')
+
+        plt.xlabel('Feature No.')
+        plt.ylabel('Mean feature value')
+        plt.title('Same class ' + str(acc_class[i][2]) + ' in two dataset')
+        plt.legend()
+        plt.savefig(data_name + ' ' + str(acc_class[i][2]))
+
+        plt.clf()
+        plt.cla()
+
 
 def plot_std_line(idata, data_name):
     """
@@ -126,12 +158,13 @@ def plot_std_line(idata, data_name):
     plt.show()
 
 if __name__ == '__main__':
-    #root_split = 'F:\hsi_result\original\KSC\data'
+    """
+    #plot misclassification
     root_split = 'I:\\try\\result_data\KSCdata'
     root_original = 'D:\hsi\dataset\Kennedy Space Center (KSC)'
 
-    data_o = sio.loadmat(root_original + '\KSCData.mat')
-    data = data_o['DataSet']
+    #data_o = sio.loadmat(root_original + '\KSCData.mat')
+    #data = data_o['DataSet']
     label_o = sio.loadmat(root_original + '\KSCGt.mat')
     label = label_o['ClsID']
     #scaler = sp.StandardScaler().fit(data)
@@ -156,5 +189,18 @@ if __name__ == '__main__':
     plot_misclassify_mean(test_mean, te_mis_class, te_mis_bands, 'KSC_test')
 
     sio.savemat(root_split + '\misData_info.mat', {'tr_mis_class': tr_mis_class, 'tr_mis_position': tr_mis_position, 'tr_mis_bands': tr_mis_bands, 'tr_mis_neighbors': tr_mis_neighbors, 'train_conf_matrix': train_conf_matrix, 
-                                                   'te_mis_class': te_mis_class, 'te_mis_position': te_mis_position, 'te_mis_bands': te_mis_bands, 'te_mis_neighbors': te_mis_neighbors, 'test_conf_matrix': test_conf_matrix})
+                                                   'te_mis_class': te_mis_class, 'te_mis_position': te_mis_position, 'te_mis_bands': te_mis_bands, 'te_mis_neighbors': te_mis_neighbors, 'test_conf_matrix': test_conf_matrix})"""
+
+    #plot according calss
+    pu_train_data, pu_train_label, _, _, pu_test_data, pu_test_label, _, _ = rd.get_data('pu_data1.mat')
+    pu_train_mean, _ = get_statistic_by_class(pu_train_data, pu_train_label)
+    pu_test_mean, _ = get_statistic_by_class(pu_test_data, pu_test_label)
+    pc_train_data, pc_train_label, _, _, pc_test_data, pc_test_label, _, _ = rd.get_data('pc_data1.mat')
+    pc_train_mean, _ = get_statistic_by_class(pc_train_data, pc_train_label)
+    pc_test_mean, _ = get_statistic_by_class(pc_test_data, pc_test_label)
+    acc_class = [[1, 3, 'Trees'], [2, 1, 'Meadows'], [4, 5, 'Bare soil'], 
+                 [5, 0, 'Asphalt'], [6, 6, 'Bitumen'], [8, 8, 'Shadows']]
+    plot_diffData_sameClass(pc_train_mean, pu_train_mean, acc_class, 'train')
+    plot_diffData_sameClass(pc_test_mean, pu_test_mean, acc_class, 'test')
+    
     print('Done.') 
