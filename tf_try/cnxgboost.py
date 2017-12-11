982c952973de937b0376f14c7ed2d3f7d5c0e69e
Luoadore <luoluomayday@gmail.com>
2017-12-11 22:17:42 +0800

    use cnn-fc as feature to train cgboost

--- tf_try/cnxgboost.py
+++ tf_try/cnxgboost.py
@@ -3,14 +3,16 @@
 Use the full-connection layer's output of CNN network as feature, feeding into xgboost train and classify.
 """
 
-#import python_analyze.read_data as rd
-import read_data as rd
+
+import python_analyze.read_data as rd
 import xgboost as xgb
 import numpy as np
 
 
 # 获得实验数据
-_, tr_lab, tr_pred, tr_data, _, te_lab, te_pred, te_data = rd.get_data('F:\\tf-try\\result\\data.mat')
+_, tr_lab, tr_pred, tr_data, _, te_lab, te_pred, te_data = rd.get_data('F:\\tf-try\\result\data.mat')
+#te_lab = np.transpose(te_lab).reshape(np.size(te_pred, 1)).astype(np.int8)
+print((tr_data).shape)
 dtrain = xgb.DMatrix(tr_data, label=np.transpose(tr_lab))
 dtest = xgb.DMatrix(te_data, label=np.transpose(te_lab))
 
@@ -19,13 +21,14 @@ param['objective'] = 'multi:softmax'
 param['eta'] = 0.1
 param['max_depth'] = 6
 param['silent'] = 1
-param['nthread'] = 1
+param['nthread'] = 4
 param['num_class'] = 13
+#param['eval_metric'] = 'mlogloss'  #mlogloss or merror used for multi-class classfication
 
 watchlist = [(dtest, 'test'), (dtrain, 'train')]
 
 # 训练分类器
-num_round = 5
+num_round = 100
 bst = xgb.train(param, dtrain, num_round, watchlist)
 
 # 测试分类器
@@ -33,7 +36,9 @@ pred = bst.predict(dtest)
 print(pred)
 print(te_lab)
 error_rate = np.sum([pred != te_lab]) / te_lab.shape[1]
+acc = np.sum([pred == te_lab]) / te_lab.shape[1]
 print('\nTest error using softmax = {}\n'.format(error_rate))
+print('\nTest accuracy using softmax = {}\n'.format(acc))
 
 # 使用输出分类概率
 param['objective'] = 'multi:softprob'
@@ -42,4 +47,6 @@ bst = xgb.train(param, dtrain, num_round, watchlist)
 pred_prob = bst.predict(dtest).reshape(te_lab.shape[1], 13)
 pred_label = np.argmax(pred_prob, axis=1)
 error_rate = np.sum(pred_label != te_lab) / te_lab.shape[1]
+acc = np.sum([pred == te_lab]) / te_lab.shape[1]
 print('\nTest error using softprob = {}\n'.format(error_rate))
+print('\nTest accuracy using softmax = {}\n'.format(acc))
