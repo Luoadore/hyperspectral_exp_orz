0d71c9b64da5b2cab44b6940b5e8e3948c853118
Luoadore <luoluomayday@gmail.com>
2017-12-12 22:10:47 +0800

    use cnn-conv output as feature to train xgboost

--- tf_try/cnxgboost.py
+++ tf_try/cnxgboost.py
@@ -10,8 +10,10 @@ import numpy as np
 
 
 # 获得实验数据
-_, tr_lab, tr_pred, tr_data, _, te_lab, te_pred, te_data = rd.get_data('F:\\tf-try\\result\data.mat')
+_, tr_lab, tr_pred, tr_data, _, te_lab, te_pred, te_data = rd.get_data('F:\\tf-try\\result\\exp1\data.mat')
 #te_lab = np.transpose(te_lab).reshape(np.size(te_pred, 1)).astype(np.int8)
+tr_data = tr_data.reshape(tr_data.shape[0], -1)
+te_data = te_data.reshape(te_data.shape[0], -1)
 print((tr_data).shape)
 dtrain = xgb.DMatrix(tr_data, label=np.transpose(tr_lab))
 dtest = xgb.DMatrix(te_data, label=np.transpose(te_lab))
@@ -19,11 +21,13 @@ dtest = xgb.DMatrix(te_data, label=np.transpose(te_lab))
 param = {}
 param['objective'] = 'multi:softmax'
 param['eta'] = 0.1
-param['max_depth'] = 6
+param['max_depth'] = 3
 param['silent'] = 1
-param['nthread'] = 4
+#param['nthread'] = 4 运行的最大线程数
 param['num_class'] = 13
 #param['eval_metric'] = 'mlogloss'  #mlogloss or merror used for multi-class classfication
+#param['scale_pos_weight'] = 1
+param['colsample_bytree'] = 0.9
 
 watchlist = [(dtest, 'test'), (dtrain, 'train')]
 
@@ -39,6 +43,10 @@ error_rate = np.sum([pred != te_lab]) / te_lab.shape[1]
 acc = np.sum([pred == te_lab]) / te_lab.shape[1]
 print('\nTest error using softmax = {}\n'.format(error_rate))
 print('\nTest accuracy using softmax = {}\n'.format(acc))
+pred = bst.predict(dtrain)
+acc = np.sum([pred == tr_lab]) / tr_lab.shape[1]
+print('\nTrain accuracy using softmax = {}\n'.format(acc))
+
 
 # 使用输出分类概率
 param['objective'] = 'multi:softprob'
@@ -47,6 +55,15 @@ bst = xgb.train(param, dtrain, num_round, watchlist)
 pred_prob = bst.predict(dtest).reshape(te_lab.shape[1], 13)
 pred_label = np.argmax(pred_prob, axis=1)
 error_rate = np.sum(pred_label != te_lab) / te_lab.shape[1]
-acc = np.sum([pred == te_lab]) / te_lab.shape[1]
+acc = np.sum([pred_label == te_lab]) / te_lab.shape[1]
 print('\nTest error using softprob = {}\n'.format(error_rate))
-print('\nTest accuracy using softmax = {}\n'.format(acc))
+print('\nTest accuracy using softprob = {}\n'.format(acc))
+pred_prob = bst.predict(dtrain).reshape(tr_lab.shape[1], 13)
+pred_label = np.argmax(pred_prob, axis=1)
+acc = np.sum([pred_label == tr_lab]) / tr_lab.shape[1]
+print('\nTrain accuracy using softmax = {}\n'.format(acc))
+
+
+# 调参
+from xgboost.sklearn import XGBClassifier
+
