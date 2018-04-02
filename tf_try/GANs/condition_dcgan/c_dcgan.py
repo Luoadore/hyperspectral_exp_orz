1e8e6a6d55170ddd867745ee265af305da1d1654
Luoadore <luoluomayday@gmail.com>
2018-04-02 23:19:43 +0800

    model

---
+++ tf_try/GANs/condition_dcgan/c_dcgan.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Condition deconvolution GAN model.
+"""
+
+import tensorflow as tf
+import dcgan_ops as ops
+import numpy as np
+
+class CGAN(object):
+    def __init__(self, data, sample_dir,):
+        pass
