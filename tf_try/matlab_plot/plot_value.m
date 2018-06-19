69315eae40c919688e5eab91bedc1a7150e8e809
luoadore <luoluomayday@gmail.com>
2018-06-19 21:45:28 +0800

    add for

--- tf_try/matlab_plot/plot_value.m
+++ tf_try/matlab_plot/plot_value.m
@@ -1,8 +1,14 @@
-% load('/media/luo/result/hsi_gan_result/KSC/hsi_data11.mat')
+dir = 'C:\Users\Mayday\Desktop\model£¨6-18£©\exp_11\data';
 bands = 1:176;
-for i= 1:100
-    % plot(bands, real(i, :))
-    plot(bands, g_sample(i, :))
-    hold on
+for n = 0: 1000:9000
+    filename = [dir, num2str(n), '.mat'];
+    load(filename);
+    for i= 1:100
+        %plot(bands, data(i, :))
+        plot(bands, g_sample(i, :))
+        hold on
+    end
+    title(['11-class sample value-', num2str(n)])
+    %title('11-class value')
+    saveas(gcf, ['g_sample', num2str(n), '.jpg'])
 end
-title('11-class sample value')
